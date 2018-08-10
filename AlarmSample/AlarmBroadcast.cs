379cb2474f1e33ce305d2741d5bee75e7de6a274
SaitouRedEyes <zeca.saitou@gmail.com>
2018-08-09 23:29:49 -0300

    Xamarin Alarm Example
    This is a example of Alarm Manager use on Xamarin.Android.

---
+++ AlarmSample/AlarmBroadcast.cs
@@ -0,0 +1,46 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.Support.V4.App;
+using Android.Widget;
+using TaskStackBuilder = Android.Support.V4.App.TaskStackBuilder;
+
+namespace AlarmSample
+{
+    [BroadcastReceiver]
+    public class AlarmBroadcast : BroadcastReceiver
+    {
+        public override void OnReceive(Context context, Intent intent)
+        {
+            PendingIntent pi = PendingIntent.GetActivity(context, 0, intent, 0);
+            AlarmManager manager = (AlarmManager)context.GetSystemService(Android.Content.Context.AlarmService);
+            manager.Cancel(pi);
+
+            // When the user clicks the notification, SecondActivity will start up.
+            Intent resultIntent = new Intent(context, typeof(MainActivity));
+
+            // Construct a back stack for cross-task navigation:
+            TaskStackBuilder stackBuilder = TaskStackBuilder.Create(context);
+            stackBuilder.AddParentStack(Java.Lang.Class.FromType(typeof(MainActivity)));
+            stackBuilder.AddNextIntent(resultIntent);
+
+            // Create the PendingIntent with the back stack:            
+            PendingIntent resultPendingIntent = stackBuilder.GetPendingIntent(0, (int)PendingIntentFlags.UpdateCurrent);
+
+            //Build Notification
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
+                .SetAutoCancel(true)
+                .SetDefaults((int)NotificationDefaults.Sound)
+                .SetContentIntent(resultPendingIntent)
+                .SetContentTitle("Notification ALARM!!!")
+                .SetSmallIcon(Resource.Drawable.ic_stat_button_click)
+                .SetContentText(string.Format("You are OUT!!."));
+
+            //Send Notification
+            NotificationManager nf = (NotificationManager)context.GetSystemService(Context.NotificationService);
+            nf.Notify(1000, builder.Build());
+
+            MainActivity.CloseThis();
+        }
+    }
+}
---
+++ AlarmSample/MainActivity.cs
@@ -0,0 +1,52 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Content;
+
+namespace AlarmSample
+{
+    [Activity(Label = "AlarmSample", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        private static Activity c;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            c = this;
+
+            Button buttonDelayAlarm = FindViewById<Button>(Resource.Id.buttonDelayAlarm);
+
+            buttonDelayAlarm.Click += OnButtonDelayAlarmClicked;
+
+            SetAlarm();
+        }
+
+        private void OnButtonDelayAlarmClicked(object sender, System.EventArgs e)
+        {
+            //Restart the Counter and Alarm
+        }
+
+        private void SetAlarm()
+        {
+            AlarmManager manager = (AlarmManager)GetSystemService(Android.Content.Context.AlarmService);
+            Intent i;
+            PendingIntent pi;
+
+            i = new Intent(this, typeof(AlarmBroadcast));
+            pi = PendingIntent.GetBroadcast(this, 0, i, 0);
+
+            manager.SetExact(AlarmType.ElapsedRealtime, SystemClock.ElapsedRealtime() + 15000, pi);
+        }
+
+        public static void CloseThis()
+        {
+            c.Finish();
+        }
+    }
+}
+
